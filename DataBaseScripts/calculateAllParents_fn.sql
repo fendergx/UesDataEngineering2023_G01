728644f1a9c6e8ceab7dfcb7e40c05af3304eace
fendergx <fendergx@gmail.com>
2023-11-13 15:14:16 -0600

    corrigiendo la funcion para calcular las jerarquias de cuentas

--- DataBaseScripts/calculateAllParents_fn.sql
+++ DataBaseScripts/calculateAllParents_fn.sql
@@ -3,8 +3,13 @@
 CREATE OR REPLACE FUNCTION calculateAllParents() RETURNS BOOLEAN AS $$
 DECLARE
     id INTEGER;
+    code VARCHAR;
+    id_company INTEGER;
     query_text TEXT;
 BEGIN
+    --borrar la informacion para limpiar
+    DELETE FROM dw_account_order where id IN (SELECT id FROM dw_account_order);
+    ALTER SEQUENCE public.dw_account_order_id_seq RESTART 1;
     -- Construir la nueva consulta para obtener la lista de fechas
     query_text := 'SELECT id,code,company_id FROM account_account ORDER BY id ASC';
     -- Iterar sobre los ids de la consulta
@@ -12,7 +17,12 @@ BEGIN
     LOOP
         -- Ejecuta calcularSaldosDiarios para cada fecha
         BEGIN
-            PERFORM findAccountParent(code,company_id);
+            -- Obtener el código de la cuenta
+            EXECUTE 'SELECT code FROM account_account WHERE id=' || id INTO code;
+            -- Obtener el id_company de la cuenta
+            EXECUTE 'SELECT company_id FROM account_account WHERE id=' || id INTO id_company;
+            -- Llamar a la función findAccountParent con los valores obtenidos
+            PERFORM findAccountParent(code, id_company);
         EXCEPTION
             WHEN OTHERS THEN
                 -- retorna falso en caso de error
