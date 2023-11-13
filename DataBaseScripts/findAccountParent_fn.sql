d005a1829b5d5fa38143dc319e727f202150c0d9
fendergx <fendergx@gmail.com>
2023-11-13 12:23:24 -0600

    agregando parametros a findAccount

--- DataBaseScripts/findAccountParent_fn.sql
+++ DataBaseScripts/findAccountParent_fn.sql
@@ -1,15 +1,17 @@
 --funcion que recibe de parametro un codigo de cuenta y retorna el codigo de cuenta padre si existe, sino retorna 0.
-CREATE OR REPLACE FUNCTION findAccountParent(codigo_inicial VARCHAR)
+CREATE OR REPLACE FUNCTION findAccountParent(codigo_inicial VARCHAR, company INTEGER)
 RETURNS VARCHAR AS
 $$
 DECLARE
     primer_digito VARCHAR;
+    id_account INTEGER;
     sub6 VARCHAR;
     sub5 VARCHAR;
     sub4 VARCHAR;
     sub3 VARCHAR;
     sub2 VARCHAR;
-BEGIN
+begin
+	 id_account := (SELECT id FROM account_account where company_id = company AND code = codigo_inicial);
     -- Obtener el primer dígito del código inicial
     primer_digito := LEFT(codigo_inicial, 1);
     -- generar los posibles codigos padre
@@ -18,20 +20,19 @@ BEGIN
     sub4 := LEFT(codigo_inicial, LENGTH(codigo_inicial) - 3) || '000';
     sub3 := LEFT(codigo_inicial, LENGTH(codigo_inicial) - 4) || '0000';
     sub2 := LEFT(codigo_inicial, LENGTH(codigo_inicial) - 5) || '00000';
-    --valida si el codigo existe en el catalogo y es diferente del codigo recibido
-	IF EXISTS (SELECT 1 FROM account_account WHERE code = sub6 and code!=codigo_inicial) THEN
+	IF EXISTS (SELECT id,company_id,code FROM account_account WHERE company_id = company AND code = sub6 and code!=codigo_inicial) THEN
 	    	RETURN sub6;
 		END IF;
-	IF EXISTS (SELECT 1 FROM account_account WHERE code = sub5 and code!=codigo_inicial) THEN
+	IF EXISTS (SELECT id,company_id,code FROM account_account WHERE company_id = company AND code = sub5 and code!=codigo_inicial) THEN
 	    	RETURN sub5;
 		END IF;
-	IF EXISTS (SELECT 1 FROM account_account WHERE code = sub4 and code!=codigo_inicial) THEN
+	IF EXISTS (SELECT id,company_id,code FROM account_account WHERE company_id = company AND code = sub4 and code!=codigo_inicial) THEN
 	    	RETURN sub4;
 		END IF;
-	IF EXISTS (SELECT 1 FROM account_account WHERE code = sub3 and code!=codigo_inicial) THEN
+	IF EXISTS (SELECT id,company_id,code FROM account_account WHERE company_id = company AND code = sub3 and code!=codigo_inicial) THEN
 	    	RETURN sub3;
 		END IF;
-	IF EXISTS (SELECT 1 FROM account_account WHERE code = sub2 and code!=codigo_inicial) THEN
+	IF EXISTS (SELECT id,company_id,code FROM account_account WHERE company_id = company AND code = sub2 and code!=codigo_inicial) THEN
 	    	RETURN sub2;
 		END IF;
     -- Devolver 0 si no hay nada
@@ -39,4 +40,4 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 --ejecucion
---select findAccountParent('101300');
+--select findAccountParent('211100');
