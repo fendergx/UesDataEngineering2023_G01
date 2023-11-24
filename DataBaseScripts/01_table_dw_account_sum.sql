0684c7141aec93a90cd59f6a92ad8311ece95d18
fendergx <fendergx@gmail.com>
2023-11-23 22:38:19 -0600

    actualizando tablas dw raw a ingles todos los campos

--- DataBaseScripts/01_table_dw_account_sum.sql
+++ DataBaseScripts/01_table_dw_account_sum.sql
@@ -1,15 +1,29 @@
 -- DROP TABLE public.dw_account_sum;
 CREATE TABLE public.dw_account_sum (
 	id serial4 NOT NULL,
-	fecha date NOT NULL,
-	id_empresa int4 NULL,
-	id_cuenta int4 not null,
-	credito numeric NULL,
-	debito numeric NULL,
+	date_sum date NOT NULL, --fecha en formato YYYY-mm-DD
+	company_id int4 not NULL,
+	company_name varchar(100) not null, --nombre de la sucursal
+	account_id int4 NOT NULL,
+	account_code varchar(64) not null, --codigo de la cuenta
+	account_name varchar(100) not null, --nombre de la cuenta
+	credit numeric not NULL,
+	debit numeric not NULL,
 	balance numeric NULL,
-	created_at timestamp default now(),
+	created_at timestamp NOT NULL default now(),
 	CONSTRAINT dw_account_sum_pk PRIMARY KEY (id)
 );
 -- public.dw_account_sum foreign keys
-ALTER TABLE public.dw_account_sum ADD CONSTRAINT dw_account_sum_id_empresa_fkey FOREIGN KEY (id_empresa) REFERENCES public.res_company(id) ON DELETE RESTRICT;
-ALTER TABLE public.dw_account_sum ADD CONSTRAINT dw_account_sum_id_cuenta_fkey FOREIGN KEY (id_cuenta) REFERENCES public.account_account(id) ON DELETE RESTRICT;
+ALTER TABLE public.dw_account_sum ADD CONSTRAINT dw_account_sum_company_id_fkey FOREIGN KEY (company_id) REFERENCES public.res_company(id) ON DELETE RESTRICT;
+ALTER TABLE public.dw_account_sum ADD CONSTRAINT dw_account_sum_account_id_fkey FOREIGN KEY (account_id) REFERENCES public.account_account(id) ON DELETE RESTRICT;
+--comentarios
+COMMENT ON COLUMN public.dw_account_sum.date_sum IS 'fecha de formato YYYY-mm-DD de la suma de los balances de la cuenta';
+COMMENT ON COLUMN public.dw_account_sum.company_id IS 'id de la sucursal de la cuenta';
+COMMENT ON COLUMN public.dw_account_sum.company_name IS 'nombre de la sucursal de la cuenta';
+COMMENT ON COLUMN public.dw_account_sum.account_id IS 'id de la cuenta asociada al registro';
+COMMENT ON COLUMN public.dw_account_sum.account_code IS 'codigo de la cuenta asociada al registro';
+COMMENT ON COLUMN public.dw_account_sum.account_name IS 'nombre de la cuenta asociada al registro';
+COMMENT ON COLUMN public.dw_account_sum.credit IS 'suma de el total de creditos para la fecha del registro';
+COMMENT ON COLUMN public.dw_account_sum.debit IS 'suma de el total de debitos para la fecha del registro';
+COMMENT ON COLUMN public.dw_account_sum.balance IS 'suma de el total de balance diario para la fecha del registro';
+COMMENT ON COLUMN public.dw_account_sum.created_at IS 'fecha y hora de la creacion del registro, este campo se llena con la informacion del momento que se registra';
--- DataBaseScripts/02_table_dw_account_order.sql
+++ DataBaseScripts/02_table_dw_account_order.sql
@@ -5,15 +5,29 @@ CREATE TABLE public.dw_account_order (
 	company_id int4 not NULL,
 	account_id int4 not null,
 	account_code varchar(64) not null,
-	--tentativo 
-    --account_type_code (primer digito)
-	has_parent boolean,
-	account_parent_id int4 null,
-	account_parent_code varchar(64),
+	account_name varchar(100) not null, 
+	account_type varchar(150) not null,
+	account_group varchar (15) not null, --internal group,1,2,3...
+	has_parent boolean null default null,
+	parent_account_id int4 null default null,
+	parent_account_code varchar(64) null default null,
+	parent_account_name varchar(100) null default null,
     created_at timestamp default now(),
 	CONSTRAINT dw_account_order_pk PRIMARY KEY (id)
 );
 -- public.dw_account_sum foreign keys
 ALTER TABLE public.dw_account_order ADD CONSTRAINT dw_account_order_company_id_fkey FOREIGN KEY (company_id) REFERENCES public.res_company(id) ON DELETE RESTRICT;
 ALTER TABLE public.dw_account_order ADD CONSTRAINT dw_account_order_account_id_fkey FOREIGN KEY (account_id) REFERENCES public.account_account(id) ON DELETE RESTRICT;
-ALTER TABLE public.dw_account_order ADD CONSTRAINT dw_account_order_account_parent_id_fkey FOREIGN KEY (account_parent_id) REFERENCES public.account_account(id) ON DELETE RESTRICT;
+ALTER TABLE public.dw_account_order ADD CONSTRAINT dw_account_order_parent_account_id_fkey FOREIGN KEY (parent_account_id) REFERENCES public.account_account(id) ON DELETE RESTRICT;
+--comentarios
+COMMENT ON COLUMN public.dw_account_order.account_id IS 'id de la cuenta en el catalogo account_account';
+COMMENT ON COLUMN public.dw_account_order.account_code IS 'codigo de la cuenta del catalogo';
+COMMENT ON COLUMN public.dw_account_order.company_id IS 'id de la sucursal de la cuenta';
+COMMENT ON COLUMN public.dw_account_order.account_name IS 'nombre de la cuenta del catalogo base';
+COMMENT ON COLUMN public.dw_account_order.account_type IS 'tipo de cuenta, esto es una sub-categoria del grupo';
+COMMENT ON COLUMN public.dw_account_order.account_group IS 'el nombre del grupo de cuenta al que pertenece la cuenta';
+COMMENT ON COLUMN public.dw_account_order.has_parent IS 'Si la cuenta tiene una cuenta padre (jerarquicamente) estará true, sino sera false';
+COMMENT ON COLUMN public.dw_account_order.parent_account_id IS 'id de la cuenta padre si la tuviere';
+COMMENT ON COLUMN public.dw_account_order.parent_account_name IS 'nombre de la cuenta padre si la tuviere';
+COMMENT ON COLUMN public.dw_account_order.parent_account_code IS 'codigo de la cuenta padre si la tuviere';
+COMMENT ON COLUMN public.dw_account_order.created_at IS 'fecha y hora de la creacion del registro, este campo se llena con la informacion del momento que se registra';
